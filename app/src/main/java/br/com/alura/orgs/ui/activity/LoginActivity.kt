9eecd0f63a7ffaed9067b558aa98fb8ac8615e74
Rafael Leonan <faelleonan@gmail.com>
2023-07-08 16:42:25 -0300

    Refatorando código

--- app/src/main/java/br/com/alura/orgs/ui/activity/LoginActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/LoginActivity.kt
@@ -9,6 +9,7 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.lifecycle.lifecycleScope
 import br.com.alura.orgs.database.AppDatabase
 import br.com.alura.orgs.databinding.ActivityLoginBinding
+import br.com.alura.orgs.extensions.toast
 import br.com.alura.orgs.extensions.vaiPara
 import br.com.alura.orgs.preferences.dataStore
 import br.com.alura.orgs.preferences.usuarioLogadoPreferences
@@ -35,19 +36,19 @@ class LoginActivity : AppCompatActivity() {
         binding.activityLoginBotaoEntrar.setOnClickListener {
             val usuarioId = binding.activityLoginUsuario.text.toString()
             val senha = binding.activityLoginSenha.text.toString()
-            lifecycleScope.launch {
-                usuarioDao.autentica(usuarioId, senha)?.let { usuario ->
-                    dataStore.edit { preferences ->
-                        preferences[usuarioLogadoPreferences] = usuario.id
-                    }
-                    vaiPara(ListaProdutosActivity::class.java)
-                    finish()
-                } ?: Toast.makeText(
-                    this@LoginActivity,
-                    "Falha na autenticação",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+            autentica(usuarioId, senha)
+        }
+    }
+
+    private fun autentica(usuarioId: String, senha: String) {
+        lifecycleScope.launch {
+            usuarioDao.autentica(usuarioId, senha)?.let { usuario ->
+                dataStore.edit { preferences ->
+                    preferences[usuarioLogadoPreferences] = usuario.id
+                }
+                vaiPara(ListaProdutosActivity::class.java)
+                finish()
+            } ?: toast("Falha na autenticação")
         }
     }
 
